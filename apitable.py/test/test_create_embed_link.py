bda1a66e600c8b63963f0af53dcc72e79fcb0540
xukecheng <cnxukecheng@gmail.com>
2023-01-04 10:07:47 +0800

    chore: update-embed-link-unitest-python

--- apitable.py/test/test_create_embed_link.py
+++ apitable.py/test/test_create_embed_link.py
@@ -2,11 +2,11 @@ import time
 import unittest
 import warnings
 from apitable import Apitable
+from apitable.types import EmbedLinkThemeEnum
 from . import DOMAIN, TOKEN, SPACE_ID, DATASHEET_ID
 
 
 class TestEmbedLinkCRD(unittest.TestCase):
-
     def setUp(self):
         warnings.simplefilter("ignore", ResourceWarning)
         apitable = Apitable(TOKEN)
@@ -15,7 +15,30 @@ class TestEmbedLinkCRD(unittest.TestCase):
 
     def test_embed_link_crd(self):
         datasheet = self.spc.datasheet(DATASHEET_ID)
-        newEmbedLink = datasheet.create_embed_link()
+        newEmbedLink = datasheet.create_embed_link(
+            {
+                "paylod": {
+                    "primarySideBar": {"collapsed": False},
+                    "viewControl": {
+                        "viewId": "viwBBc0PMN3WB",
+                        "tabBar": False,
+                        "toolBar": {
+                            "basicTools": False,
+                            "shareBtn": False,
+                            "widgetBtn": False,
+                            "apiBtn": False,
+                            "formBtn": False,
+                            "historyBtn": False,
+                            "robotBtn": False,
+                        },
+                        "collapsed": False,
+                    },
+                    "bannerLogo": True,
+                    "permissionType": "readOnly",
+                },
+                "theme": EmbedLinkThemeEnum.Light,
+            }
+        )
         self.assertIsNotNone(newEmbedLink.linkId)
         time.sleep(0.5)
         embedLinks = datasheet.get_embed_links()
